e0cf4066f568cab8d66a8c4ed8b6810526d254a4
Richard Ashley <rashley@gmail.com>
2018-06-11 02:38:43 +0100

    Added TCS

--- pushTCSdata.py
+++ pushTCSdata.py
@@ -4,24 +4,18 @@ import configHelper, generalUtils, fitsObjects
 
 defaultConfiguration = {
 	"webPath": "/home/rashley/webLogger/www",
-	"rootDataPath": "/home/rashley/webLogger/"   		# This is will be /obsdata in the production version
 	}
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='Reads the files in an ING obsdata folder an triggers the other programs to create the webLogger files.')
-	parser.add_argument('-d', '--date', type=str, default='today', help='The date to the run. Format is YYYYMMDD. Leave blank to perform for ''today'' (starting at the most recent midday).')
-	parser.add_argument('telescope', type=str, help='Telescope. Options are WHT or INT.')
+	parser = argparse.ArgumentParser(description='Polls the Parameter Notice Board at the WHT and publishes a JSON file for use on the web interfaces.')
 	parser.add_argument('--show', action='store_true', help='Showed stored configuration and exit.')
 	parser.add_argument('--clear', action='store_true', help='Clear stored configuration and exit.')
 	parser.add_argument('--set', type=str, nargs='*', help='Set a parameter.')
 	parser.add_argument('--clean', action='store_true', help='Clean out the database and regenerate all of the metadata.')
-	parser.add_argument('--force', action='store_true', help='Force regeneration of the png and thumbnails.')
-	parser.add_argument('--noimages', action='store_true', help='Don''t process the image data.')
-	parser.add_argument('--copy', action='store_true', help='Copy original FITS files to the web archive.')
-
 	arg = parser.parse_args()
 	debug = False
+	
 	installPath = os.path.dirname(os.path.realpath(__file__))
 
 	config = configHelper.config(debug = False)
@@ -47,58 +41,17 @@ if __name__ == "__main__":
 		config.save()
 		sys.exit()
 
-	if arg.date=="today":
-		logDateTime = datetime.datetime.now()
-		if logDateTime.hour<12:
-			logDateTime = logDateTime - datetime.timedelta(days=1)
-		logDate = logDateTime.strftime("%Y%m%d")
-	else: logDate = arg.date
+	noticeBoardCommand = ["ParameterNoticeBoardLister | grep TCS."]
+	print("Executing: " + str(noticeBoardCommand))
+	response = subprocess.getoutput(noticeBoardCommand)
 
-	if arg.telescope=="WHT":
-		telPath = 'whta'
-	elif arg.telescope=="INT":
-		telPath = 'inta'
-	else:
-		print("Please specify either 'INT' or 'WHT' for the telescope name.")
-		sys.exit()
-
-	print("Performing night log generation for %s on %s"%(arg.telescope,logDate))
-
-	dataPath = os.path.join(config.rootDataPath, telPath, logDate)
-	print("Looking for files in : %s"%dataPath)
-
-	outputPath = os.path.join(config.webPath, telPath, logDate)
-	print("Writing webLogger files to: %s"%outputPath)
-
-	metadataCommand = [installPath + "/getMetadata.py"]
-	metadataCommand.append('-f')
-	metadataCommand.append(dataPath)
-	metadataCommand.append('-o')
-	metadataCommand.append(outputPath)
-	if arg.clean:
-		print("Performing clean")
-		metadataCommand.append('--clean')
-
-	print("Executing: " + str(metadataCommand))
-	subprocess.call(metadataCommand)
-	if arg.clean: sys.exit()
-
-	if arg.noimages: sys.exit()
-
-	imageCommand = [installPath + "/getImageData.py"]
-	imageCommand.append('-f')
-	imageCommand.append(outputPath)
-	if arg.force:
-		print("Forcing image regeneration")
-		imageCommand.append('--force')
+	for line in response.split('\n'):
+		fields = line.split('->')
+		parameter = fields[0].strip()[4:]
+		typevalue = fields[1].strip().split(' ')
+		datatype = typevalue[0]
+		value = typevalue[1]
+		print("Parameter %s : %s  (%s)"%(parameter, value, datatype))
 
 
-	subprocess.call(imageCommand)
-
-	if arg.copy:
-		fitsCopyCommand = [installPath + "/copyOriginals.py"]
-		fitsCopyCommand.append('-o')
-		fitsCopyCommand.append(outputPath)
-		fitsCopyCommand.append('-f')
-		fitsCopyCommand.append(outputPath)
-		subprocess.call(fitsCopyCommand)
+	
